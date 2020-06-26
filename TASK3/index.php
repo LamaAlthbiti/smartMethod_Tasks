fd47a00eda4ed33862fef73c357bb6d52ff0727f
LamaHassan <lamahassanalthbiti@gmail.com>
2020-06-26 09:54:11 +0300

    upload new project
    by Lama - 26/6/2020

---
+++ TASK3/index.php
@@ -0,0 +1,94 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,"smarmethods_tasks");
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content ="TASK3 SMART METHODS">
+        <title>TASK3 SMART METHODS</title>
+        <link type="text/CSS" rel="stylesheet" href="style.css"/>
+    </head>
+
+    <body>
+ 
+        <form action="index.php" method="post">
+            <div> 
+                <?php 
+                //TOP
+                    if(isset($_POST["top"]))
+                    {
+                    $sql = "INSERT INTO control_panel2 (control) VALUES ('F')";
+                        if (!$conn->query($sql)) {
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                        }
+                    }
+                //LEFT
+                    elseif(isset($_POST["left"]))
+                    {
+                        $sql = "INSERT INTO control_panel2 (control) VALUES ('L')";
+                        
+                        if (!$conn->query($sql)) {
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+                    }
+                //RIGHT    
+                    elseif(isset($_POST["right"]))
+                    {
+                        $sql = "INSERT INTO control_panel2 (control) VALUES ('R')";
+                        
+                        if (!$conn->query($sql)) {
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+                    }
+
+                    //DOWN    
+                    elseif(isset($_POST["down"]))
+                    {
+                        $sql = "INSERT INTO control_panel2 (control) VALUES ('B')";
+                        
+                        if (!$conn->query($sql)) {
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+                    }    
+
+                    //STOP    
+                    elseif(isset($_POST["stop"])){
+                        $sql = "INSERT INTO control_panel2 (control) VALUES ('S')";
+                        
+                        if (!$conn->query($sql)) {
+                            echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+                    }    
+     
+                        //PRINT THE STORED VALUE
+                        $sql = "SELECT control FROM control_panel2 ORDER BY num DESC LIMIT 1";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                        $row = $result->fetch_assoc();
+                        echo sprintf("<p> MOVE TO : %s </p>", $row["control"]);
+                        }
+                        else {
+                        echo "0 results";
+                        }
+                        $conn->close();
+                ?>
+        </div>        
+        </form>
+
+    </body>
+
+</html>
+
+
